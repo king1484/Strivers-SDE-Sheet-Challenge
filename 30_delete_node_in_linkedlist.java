8a040090a661cb6f71d2e2a29add712c1d0a6bed
Vinay Singirikonda <85832194+king1484@users.noreply.github.com>
2023-06-08 22:24:49 +0530

    Create 30_delete_node_in_linkedlist.java
    Time Complexity - O(1)
    Space Complexity - O(1)

---
+++ 30_delete_node_in_linkedlist.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.* ;
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+        LinkedListNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+*****************************************************************/
+
+public class Solution {
+	public static void deleteNode(LinkedListNode<Integer> node) {
+		    int val = node.next.data;
+        node.data = val;
+        node.next = node.next.next;
+	}
+}
